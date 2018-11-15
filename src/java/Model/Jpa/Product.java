879baccb2b6ca2dc571ab07388387424fc99bdfe
User <User@192.168.2.39>
2018-11-15 14:06:55 +0700

    Update Jpa and Controller

---
+++ src/java/Model/Jpa/Product.java
@@ -0,0 +1,156 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model.Jpa;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author User
+ */
+@Entity
+@Table(name = "PRODUCT")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Product.findAll", query = "SELECT p FROM Product p")
+    , @NamedQuery(name = "Product.findByProductid", query = "SELECT p FROM Product p WHERE p.productid = :productid")
+    , @NamedQuery(name = "Product.findByProductname", query = "SELECT p FROM Product p WHERE p.productname = :productname")
+    , @NamedQuery(name = "Product.findByProductprice", query = "SELECT p FROM Product p WHERE p.productprice = :productprice")
+    , @NamedQuery(name = "Product.findByProductquantity", query = "SELECT p FROM Product p WHERE p.productquantity = :productquantity")
+    , @NamedQuery(name = "Product.findByProductdetail", query = "SELECT p FROM Product p WHERE p.productdetail = :productdetail")})
+public class Product implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "PRODUCTID")
+    private Integer productid;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 75)
+    @Column(name = "PRODUCTNAME")
+    private String productname;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "PRODUCTPRICE")
+    private double productprice;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "PRODUCTQUANTITY")
+    private int productquantity;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 300)
+    @Column(name = "PRODUCTDETAIL")
+    private String productdetail;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "productid")
+    private List<History> historyList;
+
+    public Product() {
+    }
+
+    public Product(Integer productid) {
+        this.productid = productid;
+    }
+
+    public Product(Integer productid, String productname, double productprice, int productquantity, String productdetail) {
+        this.productid = productid;
+        this.productname = productname;
+        this.productprice = productprice;
+        this.productquantity = productquantity;
+        this.productdetail = productdetail;
+    }
+
+    public Integer getProductid() {
+        return productid;
+    }
+
+    public void setProductid(Integer productid) {
+        this.productid = productid;
+    }
+
+    public String getProductname() {
+        return productname;
+    }
+
+    public void setProductname(String productname) {
+        this.productname = productname;
+    }
+
+    public double getProductprice() {
+        return productprice;
+    }
+
+    public void setProductprice(double productprice) {
+        this.productprice = productprice;
+    }
+
+    public int getProductquantity() {
+        return productquantity;
+    }
+
+    public void setProductquantity(int productquantity) {
+        this.productquantity = productquantity;
+    }
+
+    public String getProductdetail() {
+        return productdetail;
+    }
+
+    public void setProductdetail(String productdetail) {
+        this.productdetail = productdetail;
+    }
+
+    @XmlTransient
+    public List<History> getHistoryList() {
+        return historyList;
+    }
+
+    public void setHistoryList(List<History> historyList) {
+        this.historyList = historyList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (productid != null ? productid.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Product)) {
+            return false;
+        }
+        Product other = (Product) object;
+        if ((this.productid == null && other.productid != null) || (this.productid != null && !this.productid.equals(other.productid))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Model.Jpa.Product[ productid=" + productid + " ]";
+    }
+    
+}
